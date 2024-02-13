efbd6a8942637683b3b06b6fa462521b158b0b36
Eduard Klygunov <nostrumee@gmail.com>
2024-02-13 18:59:55 +0300

    add spring data jdbc and migrations

--- src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
+++ src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
@@ -3,6 +3,7 @@ package com.polarbookshop.catalogservice.web;
 import com.polarbookshop.catalogservice.domain.BookNotFoundException;
 import com.polarbookshop.catalogservice.domain.BookService;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
